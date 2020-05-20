6d2b015192fa0d374cfe94e01ae640ed9cb67593
Mehmet Salih Koçak <kocaksalihm@gmail.com>
2020-05-20 15:58:03 +0300

    feat: add torque and magnus effect

---
+++ Scripts/KickFottball.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickFottball : MonoBehaviour {
+  [SerializeField] private Vector3 forceToAdd;
+  [SerializeField] private Vector3 torqueToAdd;
+
+  private Rigidbody _rigidbody;
+
+  void Start() {
+    _rigidbody = GetComponent<Rigidbody>();
+  }
+
+  private void OnMouseDown() {
+    _rigidbody.AddForce(forceToAdd);
+    _rigidbody.AddTorque(torqueToAdd);
+  }
+}
---
+++ Scripts/MagnusEffector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnusEffector : MonoBehaviour {
+  [SerializeField] private float magnusConstant;
+  
+  private Rigidbody _rigidBody;
+
+  void Start() {
+    _rigidBody = GetComponent<Rigidbody>();
+  }
+
+  private void FixedUpdate() {
+    _rigidBody.AddForce(magnusConstant * Time.deltaTime * Vector3.Cross(_rigidBody.angularVelocity,
+                                                      _rigidBody.velocity));
+  }
+}
